c70a03963e60e0234df4ff78be51140e987e4838
R. G. Esteves <rgesteves@gmail.com>
2024-11-27 20:44:38 -0800

    Real validation test (of simple annotation)

--- test/validate-test.js
+++ test/validate-test.js
@@ -1,6 +1,16 @@
 var test = require('tape');
 const validateAnnotation = require('validate-web-annotation');
 
+const targetIri = 'http://example.com/page1'
+const context = 'http://www.w3.org/ns/anno.jsonld'
+const basicAnnotation = {
+  '@context': context,
+  id: 'http://example.org/anno1',
+  type: 'Annotation',
+  target: targetIri,
+}
+
 test('validation test', function(t) {
-    t.equal(true, true);
+    // t.equal(true, true);
+    t.ok(validateAnnotation(basicAnnotation));
 });
